2b81b4bc5d9766b3b09b0e7f2d1a8ece5dbfa93d
Aleksey Kosylo <alekskosylo@gmail.com>
2023-02-20 18:24:41 +0400

    Initial Commit

---
+++ DXFSwiftFramework/DXFSwiftFramework.h
@@ -0,0 +1,18 @@
+//
+//  DXFSwiftFramework.h
+//  DXFSwiftFramework
+//
+//  Created by Aleksey Kosylo on 20.02.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DXFSwiftFramework.
+FOUNDATION_EXPORT double DXFSwiftFrameworkVersionNumber;
+
+//! Project version string for DXFSwiftFramework.
+FOUNDATION_EXPORT const unsigned char DXFSwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DXFSwiftFramework/PublicHeader.h>
+
+
